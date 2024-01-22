900b9836d768603821f673ac0054e27126c9a2b8
Kyle Hipolito <kylehipolito2109@gmail.com>
2024-01-22 14:44:14 +0800

    feat: Setup basic HTTP API with dynamodb

---
+++ createCustomer.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+
+module.exports.createCustomer = async (event) => {
+  const body = JSON.parse(event.body)
+  const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+  const putParams = {
+    TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
+    Item: {
+      primary_key: body.name,
+      email: body.email
+    }
+  }
+
+  await dynamoDb.put(putParams).promise()
+
+  return {
+    statusCode: 201,
+  }
+}
